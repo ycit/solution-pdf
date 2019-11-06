68a7a893a8b10aa784e43e083677d61286083ee8
xlch <ycitcxl@163.com>
2019-11-06 21:47:26 +0800

    word2pdf

---
+++ src/main/java/com/ycit/utils/PdfUtils.java
@@ -0,0 +1,51 @@
+package com.ycit.utils;
+
+import com.aspose.words.Document;
+import com.aspose.words.License;
+import com.aspose.words.SaveFormat;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+/**
+ * @author uk
+ * 2019/11/6 20:49
+ */
+public class PdfUtils {
+
+    public static void main(String[] args) {
+        if (!getLicense()) { // 验证License 若不验证则转化出的pdf文档会有水印产生
+            return;
+        }
+        try {
+            String wordPath = "D:\\徐姗姗简历.docx";
+            String pdfPath = "D:\\1.pdf";
+            long old = System.currentTimeMillis();
+            File file = new File(pdfPath); //新建一个空白pdf文档
+            FileOutputStream os = new FileOutputStream(file);
+            Document doc = new Document(wordPath); //Address是将要被转化的word文档
+            doc.save(os, SaveFormat.PDF);//全面支持DOC, DOCX, OOXML, RTF HTML, OpenDocument, PDF, EPUB, XPS, SWF 相互转换
+            long now = System.currentTimeMillis();
+            System.out.println("共耗时：" + ((now - old) / 1000.0) + "秒"); //转化用时
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean getLicense() {
+        boolean result = false;
+        try {
+            InputStream is = PdfUtils.class.getClassLoader().getResourceAsStream("license.xml"); // license.xml应放在…\WebRoot\WEB-INF\classes路径下
+//            InputStream is =
+            License aposeLic = new License();
+            aposeLic.setLicense(is);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
